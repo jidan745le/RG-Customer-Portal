bbf4e0bf954f9f61a8e72266bf8b9bb21282fc93
jidan745le <420511176@qq.com>
2025-04-29 15:19:38 +0800

    feat:update

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -13,11 +13,9 @@ export const AuthProvider = ({ children }) => {
     const location = useLocation();
 
     useEffect(() => {
-        alert(1)
         const params = new URLSearchParams(location.search);
         const returnUrlParam = params.get('returnUrl');
         if (returnUrlParam) {
-            alert(returnUrlParam)
             setReturnUrl(returnUrlParam);
             console.log('returnUrlParam', returnUrlParam);
         }
@@ -60,7 +58,6 @@ export const AuthProvider = ({ children }) => {
 
             // Handle redirection based on returnUrl
             if (returnUrl) {
-                alert(returnUrl)
                 setTimeout(() => {
                     window.location.href = returnUrl;
                 });
